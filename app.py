dd830b9a893622386dd4174300e81162a50ad09b
Mitchell Henke <mitchh23@gmail.com>
2017-11-21 18:10:15 -0600

    test
    Former-commit-id: b0ef0c88f73b1a2ef33bb5fc15054f1a2c4a178e

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ from rnn_model.neural_networks.recurrent_layers import RecurrentLayers
 
 app = Flask(__name__)
 Compress(app)
-model = load_model("./model/model.hf5", compile=False)
-updater = Adam(learning_rate = 0.001, beta1 = 0.9, beta2 = 0.999)
-target_selection=SelectTargets(n_targets=1, shuffle=False, bias=-1.0, determinist_test=True)
-sequence_noise = SequenceNoise(dropout=0.0, swap=0.0, ratings_perturb=0.0, shuf=0.0, shuf_std=5.0)
-recurrent_layer = RecurrentLayers(layer_type='GRU', layers=[50], bidirectional=False, embedding_size=0)
+# model = load_model("./model/model.hf5", compile=False)
+# updater = Adam(learning_rate = 0.001, beta1 = 0.9, beta2 = 0.999)
+# target_selection=SelectTargets(n_targets=1, shuffle=False, bias=-1.0, determinist_test=True)
+# sequence_noise = SequenceNoise(dropout=0.0, swap=0.0, ratings_perturb=0.0, shuf=0.0, shuf_std=5.0)
+# recurrent_layer = RecurrentLayers(layer_type='GRU', layers=[50], bidirectional=False, embedding_size=0)
 
-predictor = RNNOneHot(interactions_are_unique=True, max_length=30, diversity_bias=0.0, regularization=0.0, updater=updater, target_selection=target_selection, sequence_noise=sequence_noise, recurrent_layer=recurrent_layer, use_ratings_features=False, use_movies_features=False, use_users_features=False, batch_size=16)
-dummy_dataset = DummyDataHandler(n_items=10681)
-predictor.prepare_model(dummy_dataset)
-predictor.load("./rnn_model/model.999_nt1_nf")
+# predictor = RNNOneHot(interactions_are_unique=True, max_length=30, diversity_bias=0.0, regularization=0.0, updater=updater, target_selection=target_selection, sequence_noise=sequence_noise, recurrent_layer=recurrent_layer, use_ratings_features=False, use_movies_features=False, use_users_features=False, batch_size=16)
+# dummy_dataset = DummyDataHandler(n_items=10681)
+# predictor.prepare_model(dummy_dataset)
+# predictor.load("./rnn_model/model.999_nt1_nf")
 
 def rnn_predict(json):
     formatted = [[item['id'], item['rating']] for item in json]
